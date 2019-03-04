f8f391f34dcfdc3c14984b7102394e5a2a45a77a
JaroLisiak <jaroslav.lisiak@gmail.com>
2019-03-04 17:25:58 +0100

    Data analysis

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 rawData = pd.read_csv("data.csv")
 
 try:
-    corr = rawData.corr()
+    #corr = rawData.corr()
+    #correlation matrix
+    #sns.heatmap(corr,annot=True,center=0,robust=True,cmap="Blues")
 
-    sns.heatmap(corr,annot=True,center=0,robust=True,cmap="Blues")
+    #outliers
+    #graf = sns.boxplot(x=rawData['plantPower'])
+    #graf = sns.swarmplot(x=rawData['plantIrradiance'])
 
+    # vykon / ozarovanie plot
+    # dataa = rawData.groupby(['dayID']).sum()
+    # plt.subplot(211)
+    # plt.title("Výkon elektrárne")
+    # plt.xlabel("Deň v roku")
+    # plt.ylabel("Výkon")
+    # plt.plot(dataa['plantPower'])
+    # plt.subplot(212)
+    # plt.title("Ožarovanie článkov")
+    # plt.xlabel("Deň v roku")
+    # plt.ylabel("Hodnota ožarovania")
+    # plt.plot(dataa['plantIrradiance'])
+    # plt.subplots_adjust(wspace=50,hspace=0.1)
+
+
+    #plt.plot(data=dataa)
+    # plt.subplot(212)
+    # plt.plot(rawData['id'], rawData['plantIrradiance'], '-')
+    #plt.xlabel('ID záznamu')
+    #plt.ylabel('plantPower')
+    #print(dataa.head)
+    #sns.lineplot(data=dataa, x='dayID',y='plantPower')
+
+    #sns.pairplot(rawData)
     plt.show()
 except Exception as e:
     print(e)
