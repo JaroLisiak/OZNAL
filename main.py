ce49bd8a247dfca641f85d7430fb8ad6ecb5412c
JaroLisiak <jaroslav.lisiak@gmail.com>
2019-03-16 00:13:53 +0100

    Linear regression test

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
 rawData = pd.read_csv("data.csv")
 
 try:
+    prepdata = rawData.drop(columns=['time','date','id','recordInDay','plantIrradiance','plantTemperature'])
+
+    X_train = prepdata.drop(columns=['plantPower'])[:-589]
+    X_test = prepdata.drop(columns=['plantPower'])[-589:]
+
+    Y_train = prepdata['plantPower'][:-589]
+    Y_test = prepdata['plantPower'][-589:]
+
+    reg = linear_model.LinearRegression()
+    reg.fit(X_train,Y_train)
+    predicted = reg.predict(X_test)
+
+    # The coefficients
+    print('Coefficients: \n', reg.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f" % mean_squared_error(Y_test, predicted))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(Y_test, predicted))
+
+
+    # knn = KNeighborsRegressor()
+    # knn.fit(train[['dayID', 'hour', 'weatherCloudCover', 'weatherDewPoint', 'weatherHumidity', 'weatherPressure', 'weatherTemperature', 'weatherWindBearing', 'weatherWindSpeed']], train[['plantPower']])
+    # predicted = knn.predict(test[['dayID', 'hour', 'weatherCloudCover', 'weatherDewPoint', 'weatherHumidity',  'weatherPressure', 'weatherTemperature', 'weatherWindBearing', 'weatherWindSpeed']])
+
+    print("done")
     #corr = rawData.corr()
     #correlation matrix
     #sns.heatmap(corr,annot=True,center=0,robust=True,cmap="Blues")
@@ -38,7 +68,7 @@ try:
     #sns.lineplot(data=dataa, x='dayID',y='plantPower')
 
     #sns.pairplot(rawData)
-    plt.show()
+    # plt.show()
 except Exception as e:
     print(e)
 
